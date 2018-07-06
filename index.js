f8c94c0872139c21e6e3c3b6be20029be488f820
Sasha Khamkov <sanusart@gmail.com>
2018-07-06 15:57:12 +0300

    initial

--- index.js
+++ index.js
@@ -16,32 +16,33 @@ server.route({
   path: "/",
   handler: function(request, h) {
 return 'https://img.shields.io/badge/hi-poopoo-red.svg?longCache=true&style=flat-square&label=build';
-    const query = request.query;
 
-const colorSuccess = 'green';
-const colorFailure = 'red';
-const content = query.content || 'unknown';
-const repo = query.repo;
-const jobNumber = query.job;
-const style = query.style || 'flat-square';
-const label = query.label || 'build';
+// const query = request.query;
 
-const shieldIo = (color, outcome) => 
-`https://img.shields.io/badge/${label}-${content || outcome}-${color}.svg?longCache=true&style=${style}&label=${label}`;
+// const colorSuccess = 'green';
+// const colorFailure = 'red';
+// const content = query.content || 'unknown';
+// const repo = query.repo;
+// const jobNumber = query.job;
+// const style = query.style || 'flat-square';
+// const label = query.label || 'build';
 
-fetch('https://api.travis-ci.org/repos/' + repo).then((res) => res.json()).then((build) => {
-  fetch('https://api.travis-ci.org/builds/' + build.id).then((res) => res.json()).then((travis) => {
-    if(travis.matrix[jobNumber].result === 0) {
-      return shieldIo(colorSuccess, 'success');
-    } else if(travis.matrix[jobNumber].result === 1) {
-      return shieldIo(colorFailure, 'fail');
-    } else {
-      return shieldIo(colorFailure, 'unknown');
-    }
-  });
-});
-  }
-});
+// const shieldIo = (color, outcome) => 
+// `https://img.shields.io/badge/${label}-${content || outcome}-${color}.svg?longCache=true&style=${style}&label=${label}`;
+
+// fetch('https://api.travis-ci.org/repos/' + repo).then((res) => res.json()).then((build) => {
+//   fetch('https://api.travis-ci.org/builds/' + build.id).then((res) => res.json()).then((travis) => {
+//     if(travis.matrix[jobNumber].result === 0) {
+//       return shieldIo(colorSuccess, 'success');
+//     } else if(travis.matrix[jobNumber].result === 1) {
+//       return shieldIo(colorFailure, 'fail');
+//     } else {
+//       return shieldIo(colorFailure, 'unknown');
+//     }
+//   });
+// });
+//   }
+// });
 
 // Start the server
 async function start() {
