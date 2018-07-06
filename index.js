2b396ee61179e18a9fffaa1bbd08f41fa1ecbae7
Sasha Khamkov <sanusart@gmail.com>
2018-07-06 16:28:56 +0300

    initial

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.body('laala'))
+  .get('/', (req, res) => res.send('laala'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
