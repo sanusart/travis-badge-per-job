e3a99fb6f51e8e00b59db8c10b2d49b54d1ff66c
Sasha Khamkov <sanusart@gmail.com>
2018-07-06 10:17:51 +0300

    initial

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-/*
-// URL: /?repo=Gisto/Gisto&job=1&style=flat-square&label=OSX%20build
-*/
+"use strict";
 
-const url = new URL(document.location.href);
-const colorSuccess = 'green';
-const colorFailure = 'red';
-const content = url.searchParams.get("content");
-const repo = url.searchParams.get("repo");
-const jobNumber = url.searchParams.get("job");
-const style = url.searchParams.get("style") || 'flat-square';
-const label = url.searchParams.get("label") || 'build';
+const Hapi = require("hapi");
 
-const shieldIo = (color, outcome) => `https://img.shields.io/badge/${label}-${content || outcome}-${color}.svg?longCache=true&style=${style}&label=${label}`;
-
-fetch('https://api.travis-ci.org/repos/' + repo).then((res) => res.json()).then((build) => {
-  fetch('https://api.travis-ci.org/builds/' + build.id).then((res) => res.json()).then((travis) => {
-    if(travis.matrix[jobNumber].result === 0) {
-      console.log(shieldIo(colorSuccess, 'success'));
-    } else if(travis.matrix[jobNumber].result === 1) {
-      console.log(shieldIo(colorFailure, 'fail'));
-    } else {
-      console.log(shieldIo(colorFailure, 'unknown'))
-    }
-  });
+// Create a server with a host and port
+const server = Hapi.server({
+  host: "localhost",
+  port: proccess.env.PORT || '8080'
 });
+
+// Add the route
+server.route({
+  method: "GET",
+  path: "/hello",
+  handler: function(request, h) {
+    return "hello world";
+  }
+});
+
+// Start the server
+async function start() {
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log("Server running at:", server.info.uri);
+}
+
+start();
