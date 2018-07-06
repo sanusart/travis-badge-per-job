6f0c4a180e2fd3ed2f124ba18af6899163325843
Sasha Khamkov <sanusart@gmail.com>
2018-07-06 16:24:24 +0300

    initial

--- index.js
+++ index.js
@@ -1,50 +1,84 @@
-// "use strict";
-
-const Hapi = require("hapi");
-// require('isomorphic-fetch');
+var Hapi = require('hapi');
+var Joi = require('joi');
 
 
-// Create a server with a host and port
-const server = Hapi.server({
-  host: "localhost",
-  port: process.env.PORT || '8080'
+var internals = {};
+
+
+// Type shortcuts
+
+var S = Joi.string;
+var N = Joi.number;
+var A = Joi.array;
+
+
+
+internals.get = function (request, reply) {
+
+    reply('Success!\n');
+};
+
+
+internals.output = function (request, reply) {
+
+    reply({ myOutput: request.query.input });
+};
+
+
+internals.payload = function (request, reply) {
+
+    reply('Success!\n');
+};
+
+
+internals.echo = function (request, reply) {
+
+    reply(request.payload);
+};
+
+internals.redirect = function (request, reply) {
+
+    reply.redirect('/error');
+};
+
+internals.error = function (request, reply) {
+
+    reply('This is my error');
+};
+
+var server = new Hapi.Server(~~process.env.PORT || 3000, '0.0.0.0');
+
+server.route([
+    { method: 'GET', path: '/', config: { handler: internals.get, validate: { query: { username: S() } } } },
+    { method: 'POST', path: '/', config: { handler: internals.echo, payload: { parse: true } } },
+    { method: 'GET', path: '/admin', config: { handler: internals.get, validate: { query: { username: S().required(), password: S().required() } } } },
+    { method: 'GET', path: '/users', config: { handler: internals.get, validate: { query: { email: S().email().required().min(18) } } } },
+    { method: 'GET', path: '/config', config: { handler: internals.get, validate: { query: { choices: A().required() } } } },
+    { method: 'GET', path: '/test', config: { handler: internals.get, validate: { query: { num: N().min(0) } } } },
+    { method: 'GET', path: '/output', config: { handler: internals.output, validate: { query: { input: S().min(3) } } } },
+    { method: 'GET', path: '/users/{id}', config: { description: 'Get a user', handler: internals.get, validate: { params: { id: N().required() }, query: { name: S().description('the user name').required() } } } },
+    { method: 'GET', path: '/redirect', config: { handler: internals.redirect } },
+    { method: 'GET', path: '/error', config: { handler: internals.error } }
+]);
+
+var schema = Joi.object().keys({
+    title: S().required().invalid('director'),
+    status: S().required().valid('open', 'pending', 'close'),
+    participants: A().required().includes(S(), N()).min(2)
 });
 
-// Add the route
 server.route({
-  method: "GET",
-  path: "/",
-  handler: function(request, h) {
-return 'https://img.shields.io/badge/hi-poopoo-red.svg?longCache=true&style=flat-square&label=build';
-
-// const query = request.query;
-
-// const colorSuccess = 'green';
-// const colorFailure = 'red';
-// const content = query.content || 'unknown';
-// const repo = query.repo;
-// const jobNumber = query.job;
-// const style = query.style || 'flat-square';
-// const label = query.label || 'build';
-
-// const shieldIo = (color, outcome) => 
-// `https://img.shields.io/badge/${label}-${content || outcome}-${color}.svg?longCache=true&style=${style}&label=${label}`;
-
-// fetch('https://api.travis-ci.org/repos/' + repo).then((res) => res.json()).then((build) => {
-//   fetch('https://api.travis-ci.org/builds/' + build.id).then((res) => res.json()).then((travis) => {
-//     if(travis.matrix[jobNumber].result === 0) {
-//       return shieldIo(colorSuccess, 'success');
-//     } else if(travis.matrix[jobNumber].result === 1) {
-//       return shieldIo(colorFailure, 'fail');
-//     } else {
-//       return shieldIo(colorFailure, 'unknown');
-//     }
-//   });
-// });
-  }
+    method: 'POST',
+    path: '/users/{id}',
+    config: {
+        handler: internals.payload,
+        validate: {
+            payload: schema
+        }
+    }
 });
 
 server.start(function () {
 
-  console.log('Server started at [' + server.info.uri + ']');
+    console.log('Server started at [' + server.info.uri + ']');
 });
