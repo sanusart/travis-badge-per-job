5d42633ff2010dc4469b7c398effd2a0fcbfaaf4
Sasha Khamkov <sanusart@gmail.com>
2018-07-06 16:31:11 +0300

    initial

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.send('laala'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .get("/", (req, res) => res.send("'https://img.shields.io/badge/hi-poopoo-red.svg?longCache=true&style=flat-square&label=build'"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+// return 'https://img.shields.io/badge/hi-poopoo-red.svg?longCache=true&style=flat-square&label=build';
+
+// const query = request.query;
+
+// const colorSuccess = 'green';
+// const colorFailure = 'red';
+// const content = query.content || 'unknown';
+// const repo = query.repo;
+// const jobNumber = query.job;
+// const style = query.style || 'flat-square';
+// const label = query.label || 'build';
+
+// const shieldIo = (color, outcome) => 
+// `https://img.shields.io/badge/${label}-${content || outcome}-${color}.svg?longCache=true&style=${style}&label=${label}`;
+
+// fetch('https://api.travis-ci.org/repos/' + repo).then((res) => res.json()).then((build) => {
+//   fetch('https://api.travis-ci.org/builds/' + build.id).then((res) => res.json()).then((travis) => {
+//     if(travis.matrix[jobNumber].result === 0) {
+//       return shieldIo(colorSuccess, 'success');
+//     } else if(travis.matrix[jobNumber].result === 1) {
+//       return shieldIo(colorFailure, 'fail');
+//     } else {
+//       return shieldIo(colorFailure, 'unknown');
+//     }
+//   });
+// });
+//   }
+// });
